25ee4c57fe4ef1a9080dd85057b5f207554e319a
Sivasubramanyam A <sivasubramanyama@gmail.com>
2015-09-09 18:36:21 +0530

    remove button on nav-collapse

--- report.php
+++ report.php
@@ -200,12 +200,6 @@
     <nav class="navbar navbar-default row">
       <div class="container">
         <div class="navbar-header pull-right">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
           <a class="navbar-brand" href="logout.php">Logout</a>
         </div>
       </div>
